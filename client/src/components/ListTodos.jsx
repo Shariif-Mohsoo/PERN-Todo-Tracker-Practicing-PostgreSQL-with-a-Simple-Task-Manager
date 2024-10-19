96972f0cc36b4131f292235e25ae204beb702cbc
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-19 09:35:29 -0700

    List Todo's Component

---
+++ client/src/components/ListTodos.jsx
@@ -0,0 +1,69 @@
+import { Fragment, useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
+const ListTodos = ({ callBack, description }) => {
+  const [todos, setTodos] = useState([]);
+  //delete todo function
+
+  const deleteTodo = async (id) => {
+    try {
+      await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, [description]);
+  // console.log("Todo", todos);
+  return (
+    <Fragment>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, idx) => {
+            return (
+              <tr key={idx}>
+                <td style={{ textAlign: "start" }}>{todo.description}</td>
+                <td>
+                  <EditTodo todo={todo} callBack={callBack} />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteTodo(todo.todo_id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
